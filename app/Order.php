3b59d3847efc8395488ff2aa6583b0c35c2dc0d8
Paweł Kochanek <flagoon@o2.pl>
2018-05-06 14:59:10 +0200

    Created relation between users and orders.

--- app/Order.php
+++ app/Order.php
@@ -9,4 +9,9 @@ class Order extends Model
     protected $fillable = [
         'order-item-name', 'order-item-size', 'order-item-price', 'user-id'
     ];
+
+    public function users()
+    {
+        return $this->belongsTo('App\User', 'user-id');
+    }
 }
