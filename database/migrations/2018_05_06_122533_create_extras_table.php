993e5a1a1828aac02e763ba92d6dd53d876c738c
Paweł Kochanek <flagoon@o2.pl>
2018-05-06 14:28:19 +0200

    Create table and migration for extras.

---
+++ database/migrations/2018_05_06_122533_create_extras_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExtrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extras', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('extra-name');
+            $table->string('extra-description');
+            $table->integer('extra-price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extras');
+    }
+}
