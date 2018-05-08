478f2cfc084cf872068981d59088655133ee840e
Paweł Kochanek <flagoon@o2.pl>
2018-05-08 22:12:23 +0200

    Added seeders to run.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UserSeeder::class);
+         $this->call(PizzaSizeSeeder::class);
+         $this->call(PizzaSeeder::class);
+         $this->call(IngredientsSeeder::class);
+         $this->call(ExtrasSeeder::class);
+         $this->call(CategorySeeder::class);
     }
 }
