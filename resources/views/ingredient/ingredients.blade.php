e70ec5907e464eab2f756967db879dbf803dfff5
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-05-13 11:25:29 +0200

    Redesign ingredients view.

--- resources/views/ingredient/ingredients.blade.php
+++ resources/views/ingredient/ingredients.blade.php
@@ -2,11 +2,25 @@
 
 @section('admin-content')
     <div class="col-8">
-        <h2>Ingredients!!!!!!</h2>
-        <ul>
-            @foreach($ingredients as $ingredient)
-                <li>{{ $ingredient->ingredient_name }} - {{ $ingredient->ingredient_description }}</li>
-            @endforeach
-        </ul>
+        <h1>Ingredients!!!!!!</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @foreach($ingredients as $ingredient)
+                        <h2 class="mb-0">{{ $ingredient->ingredient_name }}
+                            <a href="#">
+                                <i class="fa fa-pencil mx-1 text-success"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-trash text-danger"></i>
+                            </a>
+                        </h2>
+                        <div>
+                            {{ $ingredient->ingredient_description }}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
