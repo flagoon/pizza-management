96444c793ae7bf9b847b1217c85e38068bb1ac2d
Unknown <pawel.kochanek@gmail.com>
2018-05-13 21:25:29 +0200

    Moved models to Model folder.

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\IngredientRequest;
-use App\Ingredient;
+use App\Model\Ingredient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Ingredient;
 use Illuminate\Http\Request;
 
 class PizzaController extends Controller
