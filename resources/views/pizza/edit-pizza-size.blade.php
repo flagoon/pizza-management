c2d8bb672217c5c5d2fd53b75a37518f49b077b4
Unknown <pawel.kochanek@gmail.com>
2018-05-20 13:58:16 +0200

    blade file to edit

--- resources/views/pizza/edit-pizza-size.blade.php
+++ resources/views/pizza/edit-pizza-size.blade.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: flagoon
- * Date: 18.05.18
- * Time: 21:38
- */
+@extends('layouts.app')
+@section('admin-content')
+    <div class="col-12">
+        <form action="/pizza-sizes/{{ $pizzaSize->size_name }}" method="POST">
+            @csrf
+            @method('put')
+            <div class="form-group">
+                <label for="size-name">Size name:</label>
+                <input type="text" class="form-control" id="size-name" name="size-name" value="{{ $pizzaSize->size_name }}">
+            </div>
+            <div class="form-group">
+                <label for="size-value">Size value:</label>
+                <input type="text" class="form-control" id="size-value" name="size-value" value="{{ $pizzaSize->size_value }}">
+            </div>
+            <div class="form-group">
+                @include('errors.form-error')
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
+
+        </form>
+    </div>
+@endsection
