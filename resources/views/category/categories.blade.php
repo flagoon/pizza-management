0b52fca4886f93134155a48b7a6a75610e7552a7
Unknown <pawel.kochanek@gmail.com>
2018-05-21 22:03:59 +0200

    View for all categories.

--- resources/views/category/categories.blade.php
+++ resources/views/category/categories.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('admin-content')
-    <div class="col-8">Categories!!</div>
+    <div class="col-12">
+        <h1>
+            Categories!!
+        </h1>
+        @foreach($categories as $category)
+            <a href="{{ route('categories.edit', ['id' => $category->id]) }}">
+                <button class="btn btn-default">{{ $category->category_name }}
+                    <form action="{{ route('categories.destroy', ['id' => $category->id]) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="button btn btn-danger"><i class="fa fa-trash"></i></button>
+                    </form>
+
+                </button>
+            </a>
+
+        @endforeach
+    </div>
 @endsection
