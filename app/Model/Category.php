bc26acd784528955f5c4cc20411f2a74533c2b7e
Unknown <pawel.kochanek@gmail.com>
2018-05-26 13:39:25 +0200

    Static function for filenames.

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -1,10 +1,17 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Model;
 
+use App\Http\Requests\CategoryRequest;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public static function prepareTitle(CategoryRequest $category): string
+    {
+        $fileName = str_replace(' ', '-', $category->category_name);
+        $fileName .= '.' . $category->category_icon->getClientOriginalExtension();
+        return $fileName;
+    }
 }
