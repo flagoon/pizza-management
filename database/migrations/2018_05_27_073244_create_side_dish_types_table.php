ad14472bf80646112c5e5b0f09b9ebddfc7288fe
Unknown <pawel.kochanek@gmail.com>
2018-05-27 10:28:19 +0200

    Small refactor

--- database/migrations/2018_05_27_073244_create_side_dish_types_table.php
+++ database/migrations/2018_05_27_073244_create_side_dish_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSideDishesTypesTable extends Migration
+class CreateSideDishTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSideDishesTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('side_dishes_types', function (Blueprint $table) {
+        Schema::create('side_dish_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('side_dish_type');
             $table->timestamps();
--- database/seeds/SideDishTypeSeeder.php
+++ database/seeds/SideDishTypeSeeder.php
@@ -11,37 +11,37 @@ class SideDishTypeSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'salad',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'drink nonalcoholic',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'drink alcoholic',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'pasta',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'dessert',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        DB::table('side_dishes_types')->insert([
+        DB::table('side_dish_types')->insert([
             'side_dish_type' => 'other',
             'created_at' => now(),
             'updated_at' => now()
