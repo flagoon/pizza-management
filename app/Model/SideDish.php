71532d6ccd5dec5221a11a0d9c9631d69fca49a4
Unknown <pawel.kochanek@gmail.com>
2018-05-29 22:22:31 +0200

    Added fillable to model.

--- app/Model/SideDish.php
+++ app/Model/SideDish.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SideDish extends Model
 {
+    protected $fillable = [
+        'side_dish_name', 'side_dish_type_id', 'side_dish_volume', 'side_dish_description', 'side_dish_price'
+    ];
+
     public function sideDishType()
     {
         return $this->belongsTo(SideDishType::class);
