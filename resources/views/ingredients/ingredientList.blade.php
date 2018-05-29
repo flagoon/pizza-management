f700dde10d85c193e8481680e3bfd5e1313678cb
Unknown <pawel.kochanek@gmail.com>
2018-05-29 23:58:31 +0200

    New blade file for showing all ingredients

--- resources/views/ingredients/ingredientList.blade.php
+++ resources/views/ingredients/ingredientList.blade.php
@@ -1,61 +1,38 @@
 @extends('layouts.app')
 
 @section('admin-content')
-    <div class="col-8">
-        <h1>Ingredients!!!!!!</h1>
-        <div class="container">
-            <div class="row">
-                @foreach($ingredients as $ingredient)
-                    <div class="col-12 row">
 
-                        <div class="mr-auto">
-                            <h2 class="mb-0">{{ $ingredient->ingredient_name }}</h2>
-                        </div>
-                        <div class="col-1">
-                            <a href="/ingredients/{{ $ingredient->id }}">
-                                <button class="btn btn-success">
-                                    <i class="fa fa-pencil"></i>
-                                </button>
-                            </a>
-                        </div>
-                        <div class="col-1">
-                            <form action="/ingredients/{{ $ingredient->id }}" method="POST">
-                                {{ method_field('DELETE') }}
-                                @csrf
-                                <button type="submit" class="btn btn-danger">
-                                    <i class="fa fa-trash"></i>
-                                </button>
-                            </form>
-                        </div>
-                        <div class="col-12">
-                            {{ $ingredient->ingredient_description }}
-                        </div>
-
-                    </div>
-                @endforeach
-                <div class="col-12 mt-4 p-4">
-                    <form action="{{ route('add-ingredient') }}" method="POST">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="ingredient_name" class="col-4">Ingredient name:</label>
-                            <input class="form-control col-8" id="ingredient_name" name="ingredient_name">
-                        </div>
-                        <div class="form-group row">
-                            <label for="ingredient_description" class="col-4">Ingredient description:</label>
-                            <textarea class="form-control col-8" id="ingredient_description" name="ingredient_description"></textarea>
-                        </div>
-                        <div class="form-group">
-                            @include('errors.form-error')
-                        </div>
-
-                        <div class="form-group row">
-                            <button class="btn btn-primary offset-4" type="submit">
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <div class="col-12">
+        <h2>Ingredients list</h2>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th rowspan="2">Ingredient name</th>
+                        <th rowspan="2">Ingredient description</th>
+                        <th colspan="{{ $sizes->count() }}">Prices</th>
+                        <th rowspan="2"></th>
+                        <th rowspan="2"></th>
+                    </tr>
+                    <tr>
+                        @foreach($sizes as $size)
+                            <th>{{ $size->size_name }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($ingredients as $ingredient)
+                        <tr>
+                            <td>{{ $ingredient->ingredient_name }}</td>
+                            <td>{{ $ingredient->ingredient_description }}</td>
+                            {{-- TODO: show empty fields --}}
+                            @foreach($ingredient->pizzaSizes as $pizzaSize)
+                            <td>
+                                {{ $pizzaSize->pivot->ingredient_size_price or '=' }}
+                            </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    </div>
 @endsection
