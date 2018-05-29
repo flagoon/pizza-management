7f8a9e97fe673cc31858eb27f2e5e7e043af38f7
Unknown <pawel.kochanek@gmail.com>
2018-05-29 23:56:43 +0200

    Removing old Ingredient stuff

--- resources/views/ingredients/ingredientList.blade.php
+++ resources/views/ingredients/ingredientList.blade.php
@@ -6,32 +6,32 @@
         <div class="container">
             <div class="row">
                 @foreach($ingredients as $ingredient)
-                <div class="col-12 row">
+                    <div class="col-12 row">
 
-                    <div class="mr-auto">
-                        <h2 class="mb-0">{{ $ingredient->ingredient_name }}</h2>
-                    </div>
-                    <div class="col-1">
-                        <a href="/ingredients/{{ $ingredient->id }}">
-                            <button class="btn btn-success">
-                                <i class="fa fa-pencil"></i>
-                            </button>
-                        </a>
-                    </div>
-                    <div class="col-1">
-                        <form action="/ingredients/{{ $ingredient->id }}" method="POST">
-                            {{ method_field('DELETE') }}
-                            @csrf
-                            <button type="submit" class="btn btn-danger">
-                                <i class="fa fa-trash"></i>
-                            </button>
-                        </form>
-                    </div>
+                        <div class="mr-auto">
+                            <h2 class="mb-0">{{ $ingredient->ingredient_name }}</h2>
+                        </div>
+                        <div class="col-1">
+                            <a href="/ingredients/{{ $ingredient->id }}">
+                                <button class="btn btn-success">
+                                    <i class="fa fa-pencil"></i>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="col-1">
+                            <form action="/ingredients/{{ $ingredient->id }}" method="POST">
+                                {{ method_field('DELETE') }}
+                                @csrf
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
                         <div class="col-12">
                             {{ $ingredient->ingredient_description }}
                         </div>
 
-                </div>
+                    </div>
                 @endforeach
                 <div class="col-12 mt-4 p-4">
                     <form action="{{ route('add-ingredient') }}" method="POST">
