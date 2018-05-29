336f91f627c5c0e3cbb665191dff57b8116a5770
Unknown <pawel.kochanek@gmail.com>
2018-05-29 22:23:00 +0200

    Controller for side dish.

--- app/Http/Controllers/SideDishController.php
+++ app/Http/Controllers/SideDishController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SideDishRequest;
 use App\Model\SideDish;
 use App\Model\SideDishType;
-use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class SideDishController extends Controller
 {
@@ -25,30 +27,40 @@ class SideDishController extends Controller
      */
     public function create()
     {
-        return view('sideDish.sideDishCreate');
+        return view('sideDish.sideDishCreate', ['dishTypes' => SideDishType::all()]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param SideDishRequest  $sideDish
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SideDishRequest $sideDish)
     {
-        // TODO: use SideDishRequest
-        dd($request);
+        DB::table('side_dishes')->insert([
+            'side_dish_name' => $sideDish->side_dish_name,
+            'side_dish_type_id' => $sideDish->side_dish_type_id,
+            'side_dish_volume' => $sideDish->side_dish_volume,
+            'side_dish_description' => $sideDish->side_dish_description,
+            'side_dish_price' => $sideDish->side_dish_price
+        ]);
+        return redirect()->route('side-dish.index');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Model\SideDish  $sideDish
-     * @return \Illuminate\Http\Response
+     * @return Collection
      */
     public function show(SideDish $sideDish)
     {
-        // TODO: decide if I want to use it
+        $sideDishNew = DB::table('side_dishes')
+            ->leftJoin('side_dish_types', 'side_dish_type_id', '=', 'side_dish_types.id')
+            ->where('side_dishes.id', '=', $sideDish->id)
+            ->get();
+        return $sideDishNew;
     }
 
     /**
@@ -59,20 +71,43 @@ class SideDishController extends Controller
      */
     public function edit(SideDish $sideDish)
     {
-        // TODO: create view for edit
-        return view('sideDish.sideDishEdit', compact($sideDish));
+        $selectedSideDish = $sideDishNew = DB::table('side_dishes')
+            ->leftJoin('side_dish_types', 'side_dishes.side_dish_type_id', '=', 'side_dish_types.id')
+            ->where('side_dishes.id', '=', $sideDish->id)
+            ->select([
+                'side_dishes.id',
+                'side_dishes.side_dish_name',
+                'side_dishes.side_dish_description',
+                'side_dishes.side_dish_volume',
+                'side_dishes.side_dish_price',
+                'side_dish_types.side_dish_type_name',
+            ])
+            ->first();
+
+        return view('sideDish.sideDishEdit', [
+            'sideDish' => $selectedSideDish,
+            'dishTypes' => SideDishType::all()
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\SideDishRequest  $request
      * @param  \App\Model\SideDish  $sideDish
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, SideDish $sideDish)
+    public function update(SideDishRequest $request, SideDish $sideDish)
     {
-        // TODO: use SideDishRequest
+        $sideDish->update([
+            'side_dish_name' => $request->side_dish_name,
+            'side_dish_type_id' => $request->side_dish_type_id,
+            'side_dish_volume' => $request->side_dish_volume,
+            'side_dish_description' => $request->side_dish_description,
+            'side_dish_price' => $request->side_dish_price
+        ]);
+
+        return redirect()->route('side-dish.index');
     }
 
     /**
