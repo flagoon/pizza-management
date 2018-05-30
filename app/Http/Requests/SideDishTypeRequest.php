6d3ae46dd136d64d8914d939f606ef9bd0ba6dec
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-05-30 23:53:21 +0200

    Change column name.

--- app/Http/Requests/SideDishTypeRequest.php
+++ app/Http/Requests/SideDishTypeRequest.php
@@ -26,12 +26,12 @@ class SideDishTypeRequest extends FormRequest
         switch ($this->method()) {
             case 'POST':
                 return [
-                    'side_dish_type' => 'required|unique:side_dish_types,side_dish_type|min:3'
+                    'side_dish_type' => 'required|unique:side_dish_types,side_dish_name|min:3'
                 ];
             case 'PUT':
             case 'PATCH':
                 return [
-                    'side_dish_type' => 'required|unique:side_dish_types,side_dish_type'.$this->id.'id|min:3'
+                    'side_dish_type' => 'required|unique:side_dish_types,side_dish_name'.$this->id.'id|min:3'
                 ];
             default:
                 return [];
