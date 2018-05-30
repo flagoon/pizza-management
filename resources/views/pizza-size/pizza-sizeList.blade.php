d3bf9dc5ec557e891cde770b1d2d5ec761988df4
Unknown <pawel.kochanek@gmail.com>
2018-05-30 22:20:41 +0200

    View for all pizza sizes.

--- resources/views/pizza-size/pizza-sizeList.blade.php
+++ resources/views/pizza-size/pizza-sizeList.blade.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: flagoon
- * Date: 30.05.18
- * Time: 21:51
- */
+@extends('layouts.app')
+@section('admin-content')
+    <div class="col-12">
+        <a href="{{ route('pizza-sizes.create') }}">
+            <button class="btn btn-primary mb-3"><i class="fa fa-plus"></i> Add new</button>
+        </a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="text-center">Id</th>
+                    <th class="text-center">Pizza size</th>
+                    <th class="text-center">Width</th>
+                    <th class="text-center">Edit</th>
+                    <th class="text-center">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($pizzaSizes as $pizzaSize)
+                <tr>
+                    <td class="text-center" style="width: 1rem">{{ $pizzaSize->id }}</td>
+                    <td class="text-center">{{ $pizzaSize->size_name }}</td>
+                    <td class="text-center">{{ $pizzaSize->size_value }}</td>
+                    <td class="text-center" style="width: 1rem">
+                        <a href="{{ route('pizza-sizes.edit', [ 'id' => $pizzaSize->id ]) }}">
+                            <button class="btn btn-success">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </a>
+                    </td>
+                    <td class="text-center" style="width: 1rem">
+                        <form action="{{ route('pizza-sizes.destroy', [ 'id' => $pizzaSize->id ]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
