952bb95b7eb026e510228a96a34dbb645732a3c0
Unknown <pawel.kochanek@gmail.com>
2018-06-01 23:03:10 +0200

    Don't show table when no sizes.

--- resources/views/pizza-size/pizza-sizeList.blade.php
+++ resources/views/pizza-size/pizza-sizeList.blade.php
@@ -4,39 +4,42 @@
         <a href="{{ route('pizza-sizes.create') }}">
             <button class="btn btn-primary mb-3"><i class="fa fa-plus"></i> Add new</button>
         </a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="text-center">Pizza size</th>
-                    <th class="text-center">Width</th>
-                    <th class="text-center">Edit</th>
-                    <th class="text-center">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($pizzaSizes->sortBy('size_value') as $pizzaSize)
-                <tr>
-                    <td class="text-center">{{ $pizzaSize->size_name }}</td>
-                    <td class="text-center">{{ $pizzaSize->size_value }}</td>
-                    <td class="text-center" style="width: 1rem">
-                        <a href="{{ route('pizza-sizes.edit', [ 'id' => $pizzaSize->id ]) }}">
-                            <button class="btn btn-success">
-                                <i class="fa fa-pencil"></i>
-                            </button>
-                        </a>
-                    </td>
-                    <td class="text-center" style="width: 1rem">
-                        <form action="{{ route('pizza-sizes.destroy', [ 'id' => $pizzaSize->id ]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger">
-                                <i class="fa fa-trash"></i>
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        @if($pizzaSizes->count() !== 0)
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="text-center">Pizza size</th>
+                        <th class="text-center">Width</th>
+                        <th class="text-center">Edit</th>
+                        <th class="text-center">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($pizzaSizes->sortBy('size_value') as $pizzaSize)
+                    <tr>
+                        <td class="text-center">{{ $pizzaSize->size_name }}</td>
+                        <td class="text-center">{{ $pizzaSize->size_value }}</td>
+                        <td class="text-center" style="width: 1rem">
+                            <a href="{{ route('pizza-sizes.edit', [ 'id' => $pizzaSize->id ]) }}">
+                                <button class="btn btn-success">
+                                    <i class="fa fa-pencil"></i>
+                                </button>
+                            </a>
+                        </td>
+                        <td class="text-center" style="width: 1rem">
+                            <form action="{{ route('pizza-sizes.destroy', [ 'id' => $pizzaSize->id ]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+
     </div>
 @endsection
