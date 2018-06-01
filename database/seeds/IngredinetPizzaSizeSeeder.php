8a04ac55cdd10222300c18f2266b9df21cf6a236
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-06-01 23:15:21 +0200

    Added seeder for pivot table.

--- database/seeds/IngredinetPizzaSizeSeeder.php
+++ database/seeds/IngredinetPizzaSizeSeeder.php
@@ -42,6 +42,7 @@ class IngredinetPizzaSizeSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
         DB::table('ingredients_pizza_sizes')->insert([
             'ingredient_id' => 2,
             'pizza_size_id' => 1,
@@ -68,6 +69,101 @@ class IngredinetPizzaSizeSeeder extends Seeder
 
         DB::table('ingredients_pizza_sizes')->insert([
             'ingredient_id' => 2,
+            'pizza_size_id' => 4,
+            'ingredient_size_price' => 25,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 3,
+            'pizza_size_id' => 1,
+            'ingredient_size_price' => 10,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 3,
+            'pizza_size_id' => 2,
+            'ingredient_size_price' => 15,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 3,
+            'pizza_size_id' => 3,
+            'ingredient_size_price' => 20,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 3,
+            'pizza_size_id' => 4,
+            'ingredient_size_price' => 25,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 4,
+            'pizza_size_id' => 1,
+            'ingredient_size_price' => 10,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 4,
+            'pizza_size_id' => 2,
+            'ingredient_size_price' => 15,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 4,
+            'pizza_size_id' => 3,
+            'ingredient_size_price' => 20,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 5,
+            'pizza_size_id' => 4,
+            'ingredient_size_price' => 25,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 5,
+            'pizza_size_id' => 1,
+            'ingredient_size_price' => 10,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 5,
+            'pizza_size_id' => 2,
+            'ingredient_size_price' => 15,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 5,
+            'pizza_size_id' => 3,
+            'ingredient_size_price' => 20,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('ingredients_pizza_sizes')->insert([
+            'ingredient_id' => 5,
             'pizza_size_id' => 4,
             'ingredient_size_price' => 25,
             'created_at' => now(),
