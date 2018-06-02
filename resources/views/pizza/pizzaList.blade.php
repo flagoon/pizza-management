846e76b6d1203ba21e2b5de13ba1fd95793e4a5d
Unknown <pawel.kochanek@gmail.com>
2018-06-02 16:01:28 +0200

    Cleaning stuff.

---
+++ resources/views/pizza/pizzaList.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 02.06.18
+ * Time: 14:45
+ */
--- resources/views/pizza/pizzas.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('admin-content')
-    <div class="col-8">pizzaaaaa!!</div>
-@endsection
