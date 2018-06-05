f9cf9a6692fec3dc8b01eafc648774eaed9bd945
Unknown <pawel.kochanek@gmail.com>
2018-06-05 23:16:05 +0200

    Adds documantation and proper types

--- app/Model/Pizza.php
+++ app/Model/Pizza.php
@@ -1,25 +1,42 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pizza extends Model
 {
-    // Builds relation with ingredients model
-    public function ingredients()
+
+    /**
+     * Builds relation with Ingredient model
+     *
+     * @return BelongsToMany
+     */
+    public function ingredients(): BelongsToMany
     {
         return $this->belongsToMany(Ingredient::class, 'ingredients_pizzas');
     }
 
-    // Build relation with category model
-    public function category()
+
+    /**
+     * Builds relation with Category model
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    // Builds relation with pizza size model
-    public function pizzaSizes()
+    /**
+     * Builds relation with PizzaSize model
+     *
+     * @return BelongsToMany
+     */
+    public function pizzaSizes(): BelongsToMany
     {
         return $this->belongsToMany(PizzaSize::class, 'pizzas_pizza_sizes')->withPivot('pizza_size_price');
     }
